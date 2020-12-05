7aa8728863cda5bac955b04a3e6ab288bfeda4e1
alifarazz <alifarazdaghi@gmail.com>
2020-12-05 13:43:19 +0330

    Add day five

---
+++ 5/aoc5.nim
@@ -0,0 +1,25 @@
+import os, sugar, sequtils, algorithm
+
+const columnCount = (3, 5)[0]
+
+func binSearch(str: string, leftChar: char): int =
+  var
+    left = 0
+    right = (1 shl str.len) - 1
+  for c in str:
+    let mid = (left + right) shr 1
+    if c == leftChar:
+      right = mid
+    else:
+      left = mid + 1
+  left
+
+when isMainModule:
+  let path = if paramCount() < 1: "input" else: paramStr(1)
+  let ids = toSeq(path.lines).map(line =>
+    binSearch(line[0 .. ^(columnCount + 1)], 'F') shl columnCount +
+    binSearch(line[^(columnCount) .. ^1], 'L')).sorted()
+  echo ids.max()
+  for i in 0 .. ids.len - 2:
+    if ids[i] + 1 != ids[i + 1]:
+      echo ids[i] + 1
