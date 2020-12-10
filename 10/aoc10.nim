e3107ab068c25b66ba266c34cc3ceaab4ac513fd
alifarazz <alifarazdaghi@gmail.com>
2020-12-10 19:06:25 +0330

    Optimize dp memory of day ten

--- 10/aoc10.nim
+++ 10/aoc10.nim
@@ -3,6 +3,7 @@ import strutils, sequtils, strformat, algorithm
 when isMainModule:
   let jolts_in = stdin.readAll()[0 .. ^2].split("\n").map(parseInt).sorted()
   let jolts = @[0] & jolts_in & @[jolts_in[^1] + 3]
+
   var
     diff1 = 0
     diff3 = 0
@@ -15,13 +16,13 @@ when isMainModule:
     prev_jolt = jolt
   echo &"Part1:\n\t1s:\t{diff1}\n\t3s:\t{diff3}\n\tprod:\t{diff1 * diff3}"
 
-
-  var dp = newSeq[uint64](jolts.len)
+  var dp = newSeq[uint64](4)
   dp[0] = 1
-  for i in 1 ..< dp.len:
-    dp[i] = dp[i - 1]
+  for i in 1 ..< jolts.len:
+    let idx = i mod 4
+    dp[idx] = dp[(i - 1) mod 4]
     if i >= 2 and jolts[i] - jolts[i - 2] <= 3:
-      dp[i] += dp[i - 2]
+      dp[idx] += dp[(i - 2) mod 4]
     if i >= 3 and jolts[i] - jolts[i - 3] <= 3:
-      dp[i] += dp[i - 3]
-  echo &"Part2:\n\t{dp[^1]}"
+      dp[idx] += dp[(i - 3) mod 4]
+  echo &"Part2:\n\t{dp[(jolts.len - 1) mod 4]}"
