41776696afbebb7888752dc68f146daee96fcd8b
alifarazz <alifarazdaghi@gmail.com>
2020-12-15 19:25:13 +0330

    Tune day fifteen

--- 15/aoc15.nim
+++ 15/aoc15.nim
@@ -1,7 +1,9 @@
 import strutils, sequtils, tables
 
-func solve(nums: openArray[int], max_iter: int): int =
-  var mem = zip(nums, toSeq(countUp(1, nums.len))).to_table
+func solve(nums: openArray[int], max_iter, init_size: int): int =
+  var mem = newTable[int, int](init_size)
+  for (k, v) in zip(nums, toSeq(countUp(1, nums.len))):
+    mem[k] = v
   var lastnum = nums[^1]
   for i in mem.len ..< max_iter:
     let newnum = i - mem.getOrDefault(lastnum, i)
@@ -11,5 +13,5 @@ func solve(nums: openArray[int], max_iter: int): int =
 
 when isMainModule:
   let nums = stdin.readLine.split(",").map(parseInt)
-  echo nums.solve(2020)
-  echo nums.solve(3_0000_000)
+  echo nums.solve(2020, 385)
+  echo nums.solve(3_0000_000, 3611295)
