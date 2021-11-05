c0cba6e689d57116d63bc0e3b17eb9b456fc54bc
alifarazz <alifarazdaghi@gmail.com>
2021-11-06 01:18:06 +0330

    2020/1/nim: Use sets insted of linear search

--- 2020/1/aoc1.nim
+++ 2020/1/aoc1.nim
@@ -1,22 +1,23 @@
-import strutils
+import strutils, intsets
 from sequtils import map
 
 when isMainModule:
   var str = readFile("input").split()
   discard str.pop()
   let data = str.map(parseInt)
+  let dataSet = data.toIntSet()
 
   block day1:
     for i in data:
-      for j in data:
-        if (i + j) == 2020:
-          echo i * j
-          break day1
+      let j = 2020 - i
+      if j in dataSet:
+        echo i * j
+        break day1
 
   block day2:
     for i in data:
       for j in data:
-        for k in data:
-          if (i + j + k) == 2020:
-            echo i * j * k
-            break day2
+        let k = 2020 - i - j
+        if k in dataSet:
+          echo i * j * k
+          break day2
