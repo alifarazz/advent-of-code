1ec9ce7c71d1446b32ee1075be1140c9b429f868
alifarazz <alifarazdaghi@gmail.com>
2021-12-01 18:45:12 +0330

    2021: Day1: Optimize Nim version some more

--- 2021/1/day1.nim
+++ 2021/1/day1.nim
@@ -4,10 +4,9 @@ from std/sequtils import map
 when isMainModule:
   let v = stdin.readAll.splitLines[0..^2].map(parseInt)
   var c1, c2: int
-  for i in 1..<v.len:
-    if v[i - 1] < v[i]:
-      inc c1
+  inc c1, int(v[0] < v[1])
+  inc c1, int(v[1] < v[2])
   for i in 3..<v.len:
-    if v[i - 3] < v[i]:
-      inc c2
+    inc c2, int(v[i - 3] < v[i])
+    inc c1, int(v[i - 1] < v[i])
   echo c1, '\n', c2
