73e566967299e8919d7cfaae49e39cc0ef6a3dc2
alifarazz <alifarazdaghi@gmail.com>
2022-12-10 06:08:06 +0330

    2022:9: Use ints
    This way, all the operations are on bits and no allocation happens for
    tuples. Exec time went from 5.2 to 2.7 secs.

--- 2022/9/d9.nim
+++ 2022/9/d9.nim
@@ -1,39 +1,56 @@
-import std/[sets, strutils]
+import std/[packedsets, strutils]
 
 const knots_len = 10
 
-type XY = (int, int)
+type XY = distinct uint64
 
-func `-`(x, y: XY): XY = (x[0] - y[0], x[1] - y[1])
-func `+`(x, y: XY): XY = (x[0] + y[0], x[1] + y[1])
-func `+=`(x: var XY, y: XY) = x = x + y
-func clamp(x: XY, a, b: int): XY = (x[0].clamp(a, b), x[1].clamp(a, b))
+proc `[]`[I: Ordinal](x: XY, i: I): int32 =
+  let ShiftAmnt = i * 32
+  let Mask = (if i == 0: 0x0000_0000_FFFF_FFFFu else: 0xFFFF_FFFF_0000_0000u)
+  let Chunk = x and Mask
+  let Result = uint64(Chunk shr ShiftAmnt)
+  result = (
+    if Result > 0x7FFF_FFFFu:
+      -int32((0x80_000_000u - (Result - 0x7fffffffu) + 1u))
+    else:
+      Result.int32
+    )
+
+proc `and`[T: Ordinal or XY](x: XY, y: T): XY = XY(uint64(x) and uint64(y))
+proc `or`[T: Ordinal or XY](x: XY, y: T): XY = XY(uint64(x) or uint64(y))
+proc `shl`[T: Ordinal](x: XY, i: T): XY = XY(uint64(x) shl i)
+proc `shr`[T: Ordinal](x: XY, i: T): XY = XY(uint64(x) shr i)
+proc toXY(x, y: int32): XY = XY(uint32(x)) or (XY(y) shl 32)
+proc `-`(x, y: XY): XY = toXY(x[0] - y[0], x[1] - y[1])
+proc `+`(x, y: XY): XY = toXY(x[0] + y[0], x[1] + y[1])
+proc `==`(x, y: XY): bool = x.uint64 == y.uint64
+proc `+=`(x: var XY, y: XY) = x = x + y
 
 proc update_head(h: var XY, dir: char) =
-  h[0] += (dir == 'R').int - (dir == 'L').int
-  h[1] += (dir == 'U').int - (dir == 'D').int
+  h = toXY(h[0] + (dir == 'R').int32 - (dir == 'L').int32,
+           h[1] + (dir == 'U').int32 - (dir == 'D').int32)
 
 proc update_tail(t: var XY, h: XY) =
-  let diff = h - t
-  if diff[0] * diff[0] + diff[1] * diff[1] > 2:
-    t += diff.clamp(-1, 1)
+  let d0 = h[0] - t[0]
+  let d1 = h[1] - t[1]
+  if d0 * d0 + d1 * d1 > 2:
+    t += toXY(d0.clamp(-1, 1), d1.clamp(-1, 1))
 
 proc main() =
   var head, tail: XY
   var knots: array[knots_len, XY]
-  var silver_seen = toHashSet[XY]([(0, 0)])
-  var gold_seen = toHashSet[XY]([(0, 0)])
+  var silver_seen, gold_seen: PackedSet[uint64]
   for line in stdin.lines():
     let dir: char = line[0]
-    let nstep: int = line[2..^1].parseInt
+    let nstep = line[2..^1].parseInt
     for i in 1..nstep:
       update_head(head, dir)
       update_tail(tail, head)
-      silver_seen.incl(tail)
+      silver_seen.incl(tail.uint64)
       update_head(knots[0], dir)
       for j in 1..<knots.len:
         update_tail(knots[j], knots[j - 1])
-      gold_seen.incl(knots[^1])
+      gold_seen.incl(knots[^1].uint64)
 
   echo silver_seen.len
   echo gold_seen.len
