e7853e7635f11eaaeccfeb90673172a88d6d6538
alifarazz <alifarazdaghi@gmail.com>
2022-12-15 10:35:45 +0330

    2022:13: Remove list() and Add tee()

--- 2022/13/d13.py
+++ 2022/13/d13.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-from operator import mul
-from functools import cmp_to_key, reduce
-from itertools import islice
+from functools import reduce
+from itertools import tee
 import json
 
 # Look Ma! No for loops! (At least, not explicitly)
@@ -13,10 +12,18 @@ def LazyReadlines(fp):
         yield line
 
 
+def LazyBundleIntoPairs(generator):
+    try:
+        while True:
+            now = next(generator)
+            after = next(generator)
+            yield (now, after)
+    except StopIteration:
+        return
+
+
 def ParseInput(Input):
-    return map(json.loads,
-               filter(lambda x: x != "\n",
-                      Input))
+    return map(json.loads, filter(lambda x: x != "\n", Input))
 
 
 def PacketCmp(p1, p2):
@@ -26,29 +33,22 @@ def PacketCmp(p1, p2):
     EncloseInListIfNeccessary = lambda x: [x] if IsInt(x) else x
     n1, n2 = map(EncloseInListIfNeccessary, [p1, p2])
     RecurseResult = map(lambda x: PacketCmp(*x), zip(n1, n2))
-    FirstOkResult = next(filter(lambda x: x != 0,
-                                RecurseResult),
-                         None)
+    FirstOkResult = next(filter(lambda x: x != 0, RecurseResult), None)
     return FirstOkResult or len(n2) - len(n1)
 
 
 def Silver(Input):
     Score = lambda PairNumber, CmpResult: (CmpResult > 0) * PairNumber
 
-    PacketPairs = zip(islice(Input, 0, None, 2),
-                      islice(Input, 1, None, 2))
-    Scores = map(lambda x: Score(x[0], PacketCmp(*x[1])),
-                 enumerate(PacketPairs, 1))
+    PacketPairs = LazyBundleIntoPairs(Input)
+    Scores = map(lambda x: Score(x[0], PacketCmp(*x[1])), enumerate(PacketPairs, 1))
     return sum(Scores)
 
 
 def Gold(Input):
     def CheckPacketIfBeforeDecoders(Pkt):
         Before2 = PacketCmp([2], Pkt) < 0
-        return (
-            Before2,
-            not Before2 and PacketCmp([6], Pkt) < 0
-        )
+        return (Before2, not Before2 and PacketCmp([6], Pkt) < 0)
 
     Position2, Position6 = reduce(
         lambda x, y: (x[0] + y[0], x[1] + y[1]),
@@ -62,9 +62,9 @@ def main():
     with open(0) as fp:
         LazyLines = LazyReadlines(fp)
         Input = ParseInput(LazyLines)
-        Input = list(Input)
-    print(Silver(Input))
-    print(Gold(Input))
+        V = [Silver, Gold]
+        Results = map(lambda x: x[0](x[1]), zip(V, tee(Input)))
+        print(*Results, sep="\n")
 
 
 if __name__ == "__main__":
