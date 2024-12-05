16f7c643a62dc55089ab0be16a4af9dd52558276
alifarazz <alifarazdaghi@gmail.com>
2024-12-05 01:17:55 -0500

    2024: Add Day5 gold

--- 2024/5/d5.cc
+++ 2024/5/d5.cc
@@ -1,8 +1,9 @@
-#include <array>
 #include <bits/stdc++.h>
-#include <cinttypes>
-#include <cstdint>
-#include <cstdio>
+
+template <int offset> std::uint8_t tonum(const char *const s) {
+  return (std::uint8_t)(s[offset + 0] - '0') * 10 +
+         (std::uint8_t)(s[offset + 1] - '0');
+}
 
 int main() {
   std::array<std::vector<std::uint8_t>, 100> rules;
@@ -11,43 +12,37 @@ int main() {
   while (fgets(rule_line.data(), rule_line.size(), stdin)) {
     if (rule_line[0] == '\n')
       break;
-    std::uint8_t a = (rule_line[0] - '0') * 10 + rule_line[1] - '0';
-    std::uint8_t b = (rule_line[3] - '0') * 10 + rule_line[4] - '0';
+    std::uint8_t a = tonum<0>(rule_line.data());
+    std::uint8_t b = tonum<3>(rule_line.data());
     rules[a].push_back(b);
   }
 
-  for (auto i : {29,47,53,61,75,97}) {
-    std::cout << i << ": ";
-    for (auto v: rules[i])
-      std::cout << (int)v << ' ';
-    std::cout << std::endl;    
-  }
-
-  std::uint32_t acc_silver = 0;
+  std::uint_fast32_t acc_silver = 0, acc_gold = 0;
   std::vector<std::uint8_t> pages;
   while (fgets(rule_line.data(), 4, stdin)) {
     if (rule_line[0] == '\n')
       break;
-    std::uint8_t v = (rule_line[0] - '0') * 10 + rule_line[1] - '0';
-    pages.push_back(v);
-    if (rule_line[2] == '\n') {
-      // calculate if its valid
-      for (auto v: pages) {
-        std::cout << (int)v << " ";
-      }
-      std::cout << std::endl;
-      
-      bool ok = true;
-      for (size_t i = 0; i < pages.size() && ok; i++) {
-        for (size_t j = i + 1; j < pages.size() && ok; j++) {
-          auto u = pages[i], v = pages[j];
-          if (std::find(rules[v].begin(), rules[v].end(), u) != rules[v].end())
-            ok = false;
+
+    pages.push_back(tonum<0>(rule_line.data()));
+
+    if (rule_line[2] == '\n') { // calculate if its valid
+      bool ok = false;
+      for (size_t i = 0; i < pages.size(); i++) {
+        for (size_t j = i + 1; j < pages.size(); j++) {
+          auto &u = pages[i], &v = pages[j];
+          if (std::find(rules[v].begin(), rules[v].end(), u) !=
+              rules[v].end()) {
+            ok = true;
+            std::swap(u, v);
+          }
         }
       }
-      acc_silver += ok ? pages[pages.size() / 2] : 0;
+      acc_silver += !ok ? pages[pages.size() / 2] : 0;
+      acc_gold   +=  ok ? pages[pages.size() / 2] : 0;
       pages.clear();
     }
   }
-  std::printf("%" PRIu32 "\n", acc_silver);
+  std::printf("%" PRIuFAST32 "\n"
+              "%" PRIuFAST32 "\n",
+              acc_silver, acc_gold);
 }
