31c74d34828c35f478b38b62c01002343c3ee86b
alifarazz <alifarazdaghi@gmail.com>
2024-12-09 22:11:35 -0500

    2024: Add Day9 gold

--- 2024/9/d9.cc
+++ 2024/9/d9.cc
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstdint>
 #include <iostream>
 #include <ranges>
 #include <string>
@@ -23,24 +24,21 @@ int main()
       size_t { 0 },
       std::plus<size_t>());
 
-  auto show_disk = [&]() {
-    for (auto c : disk) {
-      if (c == UINT32_MAX)
-        std::cout << ".";
-      else
-        std::cout << c;
-    }
-    std::cout << std::endl;
-  };
-
+  std::vector<std::pair<std::uint32_t, std::uint32_t>> freespaces, files; // begin idx, len
+  freespaces.reserve(s.length() / 2);
+  files.reserve(s.length() / 2);
   uint32_t counter = 0;
   for (size_t i = 0; i < nums.size(); i++) {
     uint32_t c = i % 2 == 1 ? UINT32_MAX : counter++;
+    if (c == UINT32_MAX) // can be reduced
+      freespaces.emplace_back(disk.size(), nums[i]);
+    else
+      files.emplace_back(disk.size(), nums[i]);
     for (uint32_t j = 0; j < nums[i]; j++)
       disk.push_back(c);
   }
 
-  {
+  auto silver = [&]() {
     size_t i = 0, j = disk.size() - 1;
     while (i < j) {
       if (disk[j] == UINT32_MAX) {
@@ -55,11 +53,47 @@ int main()
       }
       i++;
     }
-  }
+    std::uint64_t acc_silver = 0;
+    for (i = 0; i < compact_size; i++) {
+      acc_silver += i * disk[i];
+    }
+    return acc_silver;
+  };
 
-  std::uint64_t acc_silver = 0;
-  for (size_t i = 0; i < compact_size; i++) {
-    acc_silver += i * disk[i];
-  }
-  std::cout << acc_silver << std::endl;
+  auto gold = [&]() {
+    for (ssize_t j = s.size() / 2; j >= 0; j--) {
+      auto& [filestart, filelen] = files[j];
+      for (size_t i = 0; i < s.size() / 2; i++) {
+        auto& [freestart, freelen] = freespaces[i];
+        if (freestart > filestart)
+          break;
+        if (filelen <= freelen) {
+          freelen -= filelen;
+          filestart = freestart;
+          freestart += filelen;
+          break;
+        }
+      }
+    }
+    size_t acc_gold = 0;
+    for (size_t i = 0; i < files.size(); i++) {
+      for (size_t j = files[i].first; j < files[i].first + files[i].second; j++) {
+        acc_gold += i * j;
+      }
+    }
+    return acc_gold;
+  };
+
+  auto g = gold();
+  std::cout << silver() << '\n'
+            << g << std::endl;
 }
+// auto show_disk = [&]() {
+//   for (auto c : disk) {
+//     if (c == UINT32_MAX)
+//       std::cout << ".";
+//     else
+//       std::cout << c;
+//   }
+//   std::cout << std::endl;
+// };
