691545a5b0bd378ed7d1e237f5f2d18008b37d94
alifarazz <alifarazdaghi@gmail.com>
2024-12-13 02:25:16 -0500

    2024: Add Day13 gold

--- 2024/13/d13.cc
+++ 2024/13/d13.cc
@@ -1,24 +1,14 @@
-#include <algorithm>
-#include <array>
-#include <charconv>
-#include <cstdint>
 #include <iostream>
-#include <ranges>
-#include <set>
-#include <string>
-#include <vector>
-
-std::string s;
 
 int main()
 {
   std::istreambuf_iterator<char> begin(std::cin), end;
-  s = std::string(begin, end);
+  std::string s = std::string(begin, end);
 
-  size_t acc = 0;
+  std::uint64_t acc_silver = 0, acc_gold = 0;
   size_t idx = 0;
   do {
-    size_t xa, ya, xb, yb;
+    std::int64_t xa, ya, xb, yb;
     idx += 12;
     std::from_chars(s.data() + idx, s.data() + idx + 2, xa);
     idx += 6;
@@ -29,34 +19,26 @@ int main()
     std::from_chars(s.data() + idx, s.data() + idx + 2, yb);
     idx += 9 + 3;
 
-    size_t X, Y;
+    std::int64_t X, Y;
     auto fc_res = std::from_chars(s.data() + idx, s.data() + idx + 1337, X);
     idx = fc_res.ptr - s.data() + 4;
     size_t idx_nl = s.begin() - std::find(s.begin() + idx, s.end(), '\n');
     fc_res = std::from_chars(s.data() + idx, s.data() + idx_nl, Y);
     idx = fc_res.ptr - s.data() + 2;
 
-    // std::cout << xa << "\n" << ya << "\n" << xb << "\n" << yb << "\n\n";
-    // std::cout << X << "\n" << Y << "\n";
-    // std::cout << "--------------------\n";
-
-    size_t na = 0, nb = 0;
-    uint64_t cost = UINT64_MAX;
-    for (size_t i = 1; i <= 100; i++) {
-      for (size_t j = 1; j <= 100; j++) {
-        uint64_t newcost = i * 3 + j;
-        size_t x = i * xa + j * xb;
-        size_t y = i * ya + j * yb;
-        if (x == X && y == Y && cost > newcost) {
-          std::cout << "AH" << std::endl;
-          na = i;
-          nb = j;
-          cost = newcost;
+    auto cramer = [&](std::int64_t X, std::int64_t Y, std::uint64_t& acc) {
+      if (std::int64_t det = xa * yb - xb * ya; det) {
+        std::int64_t na = X * yb - xb * Y;
+        std::int64_t nb = xa * Y - X * ya;
+        if (na % det == 0 && nb % det == 0) {
+          na /= det;
+          nb /= det;
+          acc += na * 3 + nb;
         }
       }
-    }
-    // std::cout << na << '\t' << nb << "\n";
-    acc += na  * 3+ nb;
+    };
+    cramer(X, Y, acc_silver);
+    cramer(X + 10000000000000, Y + 10000000000000, acc_gold);
   } while (idx < s.size());
-  std::cout << acc;
+  std::cout << acc_silver << "\n" << acc_gold << std::endl;
 }
